569aead2544bfc73f05a79c38e65302b8f22c297
Vimran <74567639+Vimran@users.noreply.github.com>
2021-09-07 19:31:42 -0400

    lab 2 index

---
+++ labs/lab2/js/app.js
@@ -0,0 +1,20 @@
+let x = 0;
+function draw(){
+    x = x + 1;
+    console.log(x);
+}
+
+function setup(){
+    createCanvas(400,300);
+}
+function draw(){
+    background(0);
+    if(mouseX>200){
+        fill("red");
+    } else{
+        fill("blue");
+    }
+    ellipse(mouseX, mouseY, 20, 20);
+ }
+
+ 
